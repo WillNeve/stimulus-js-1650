f912c16ff4dd65dc6521842d9e7f3900a59382ac
William Neve <williamneve6000@gmail.com>
2024-05-16 11:23:21 +0100

    comments added

--- javascript/application.js
+++ javascript/application.js
@@ -1,13 +1,18 @@
+// we import the application class (top level process) from Stimulus
 import { Application } from 'stimulus';
+// we start the Stimulus top level process and bind a reference to the Stimulus object
 window.Stimulus = Application.start()
 
+// we import all of our controllers from our respective controller files
 import DisableButtonController from "./controllers/disable_button_controller.js"
+// ... and register them with stimulus, giving them a name that we can refer to them by in our HTML
 Stimulus.register("disable-button", DisableButtonController)
 
-// const button = document.querySelector("#click-me");
 
-// button.addEventListener("click", (event) => {
-//   event.target.disabled = true;
-// })
+// The VANILLA/OLD way of selecting an element
+const button = document.querySelector("#non-existant-button");
 
-
+// The VANILLA/OLD way of adding an event listener to said element
+button.addEventListener("click", (event) => {
+  event.target.disabled = true;
+})
--- javascript/controllers/disable_button_controller.js
+++ javascript/controllers/disable_button_controller.js
@@ -1,11 +1,15 @@
+// We import the base controller class (with all the base functionality) from Stimulus
 import { Controller } from 'stimulus';
 
+// We define a class that inherits from the base class and export it
 export default class extends Controller {
+  // we accept/register targets made available in our HTML
   static targets = ["button", "resetButton"];
+  // we accept/register values made available in our HTML
   static values = {
     dog: String
   }
-
+  // this function is automatically invoked/called when the controller connects
   connect() {
     console.log("Hello from our first Stimulus controller");
   }
